4558c54c0ddb2f3dad61a95fa99694ade908eb42
Alisultan <a.abdirovkz@gmail.com>
2026-06-13 00:02:01 +0500

    Add group workspace navigation

--- frontend/src/app/groups/[id]/layout.tsx
+++ frontend/src/app/groups/[id]/layout.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import GroupNav from './GroupNav';
 
 type Props = {
@@ -8,6 +10,13 @@ export default function GroupLayout({ children }: Props) {
   return (
     <main className="min-h-screen bg-gray-100 p-8">
       <div className="max-w-6xl mx-auto">
+        <Link
+          href="/"
+          className="inline-block mb-4 text-sm font-medium text-gray-600 hover:text-black"
+        >
+          Back to groups
+        </Link>
+
         <GroupNav />
         {children}
       </div>
